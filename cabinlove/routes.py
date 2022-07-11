e4ba2566e834a6122c6c08fb825867d390bac2db
berill98 <berill0407@gmail.com>
2022-07-11 05:05:00 +0000

    Create Add location page

--- cabinlove/routes.py
+++ cabinlove/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from cabinlove import app, db
 from cabinlove.models import Location, Cabin, User
 
@@ -6,3 +6,18 @@ from cabinlove.models import Location, Cabin, User
 @app.route("/")
 def home():
     return render_template("cabins.html")
+
+
+@app.route("/locations")
+def locations():
+    return render_template("locations.html")
+
+
+@app.route("/add_location", methods=["GET", "POST"])
+def add_location():
+    if request.method == "POST":
+        location = Location(location_name=request.form.get("location_name"))
+        db.session.add(location)
+        db.session.commit()
+        return redirect(url_for("locations"))
+    return render_template("add_location.html")
